613a4053238afc8bd9e82e8fd6075c60c36881c3
ileniaperrella <ilenia.perrella@pal-robotics.com>
2024-01-18 11:02:46 +0100

    migration of CMakeLists.txt and package.xml to ros2 - description

--- talos_description/test/calibration/utils/generate_urdfs.sh
+++
@@ -1,2 +0,0 @@
-rosrun xacro xacro --inorder ../../robots/talos_full_v2.urdf.xacro foot_collision:=default enable_crane:=false head_type:=default disable_gazebo_camera:=false test:=false > all_zero.urdf
-rosrun xacro xacro --inorder ../../robots/talos_full_v2.urdf.xacro foot_collision:=default enable_crane:=false head_type:=default disable_gazebo_camera:=false test:=true > displaced.urdf
